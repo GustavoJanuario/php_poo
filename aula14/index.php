01270b3153a0e3554237fddca718aa30b89414a8
Gustavo Januário <gustavo.sjanuario@gmail.com>
2022-12-24 16:49:17 -0300

    add aula14

---
+++ aula14/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Youtube</title>
+</head>
+<body>
+    <pre>
+        <?php 
+            require_once("Video.php");
+
+            $v[0] = new Video("Aula 1 de POO");
+            $v[1] = new Video("Aula 12 de PHP");
+            $v[2] = new Video("Aula 15 de Html");
+
+            print_r($v);
+        
+        ?>
+    </pre>
+</body>
+</html>
